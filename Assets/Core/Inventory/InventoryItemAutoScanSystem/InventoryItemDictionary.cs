20530cef46441554b6c2a4db6311aa5fae326002
jjnwaterpolo <jaronieuwenhuisen@gmail.com>
2025-12-12 19:21:59 +0100

    Inventory

--- Assets/Core/Inventory/InventoryItemAutoScanSystem/InventoryItemDictionary.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/InventoryItemDictionary", order = 1)]
-public class InventoryItemDictionary : ScriptableObject
-{
-    public List<InventoryItemDefinition> items = new List<InventoryItemDefinition>();
-
-}
--- Assets/Core/Inventory/InventoryItemDefinition.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "InventoryItemDefinition", menuName = "InventoryItem", order = 1)]
-public class InventoryItemDefinition : ScriptableObject
-{
-    public Sprite icon;
-}
